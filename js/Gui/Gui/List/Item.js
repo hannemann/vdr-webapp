0bdf8b979d12c9fd587d6a9783be430082b424bc
hannemann <hannemann666@gmx.net>
2014-05-01 14:47:37 +0200

    delete obsolete files

--- js/Gui/Gui/List/Item.js
+++
@@ -1,5 +0,0 @@
-VDRest.Gui.List.Item = function () {};
-
-VDRest.Gui.List.Item.prototype.mainClassName = 'list-item';
-
-VDRest.Gui.List.Item.prototype = new VDRest.Gui.Item();
--- js/Gui/Menubar/Model/Default.js
+++
@@ -1,4 +0,0 @@
-
-Gui.Menubar.Model.Default = function () {};
-
-Gui.Menubar.Model.Default.prototype = new VDRest.Abstract.Model();
