1f9d99fe0c065a71274c32d95e67a5f1dee24bd8
hannemann <hannemann666@gmx.net>
2014-05-16 22:04:15 +0200

    adds untested displaying optionally currently playing video to drawer info section

--- js/Gui/Window/View/Drawer.js
+++ js/Gui/Window/View/Drawer.js
@@ -80,20 +80,27 @@ Gui.Window.View.Drawer.prototype.addInfo = function () {
     var info = VDRest.app.getModule('VDRest.Info').getModel('Info'),
         channel, onAir = $('<div class="onAir clearer">');
 
-    channel = VDRest.app.getModule('VDRest.Epg').loadModel('Channels.Channel', info.getData('channel'));
-
     this.info = $('<div class="info"><div class="header">Info</div></div>');
 
-    if (channel.hasData('image')) {
+    if (info.hasData('channel')) {
 
-        $('<img>').attr('src', channel.getData('image')).appendTo(onAir);
+        channel = VDRest.app.getModule('VDRest.Epg').loadModel('Channels.Channel', info.getData('channel'));
 
-    } else {
+        if (channel.hasData('image')) {
 
-        $('<div>').text(channel.getData('name') + ' - ').appendTo(onAir)
+            $('<img>').attr('src', channel.getData('image')).appendTo(onAir);
+
+        } else {
+
+            $('<div>').text(channel.getData('name') + ' - ').appendTo(onAir)
+        }
+
+        $('<span>').text(info.getData('title')).appendTo(onAir);
+
+    } else if (info.hasData('video')) {
+
+        $('<div>').text(info.getData('video')).appendTo(onAir)
     }
-
-    $('<span>').text(info.getData('title')).appendTo(onAir);
 
     onAir.appendTo(this.info);
 
