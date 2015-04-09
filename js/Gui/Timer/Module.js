3e5279ff004ea9293d3274dc0b072f91953b6f7d
hannemann <hannemann666@gmx.net>
2015-04-09 17:19:47 +0200

    Timer: add ability to refresh timers

--- js/Gui/Timer/Module.js
+++ js/Gui/Timer/Module.js
@@ -40,12 +40,39 @@ Gui.Timer.prototype.startPage = true;
 Gui.Timer.prototype.headline = 'Timer';
 
 /**
+ * context menu definition
+ * @type {{}}
+ */
+Gui.Timer.prototype.contextMenu = {
+
+    "Refresh": {
+        "labels": {
+            "on": VDRest.app.translate("Refresh")
+        },
+        "state": "on",
+        "scope": 'Gui.Timer',
+        "fn": function () {
+
+            this.refresh();
+        }
+    }
+};
+
+/**
  * dispatch default view
  */
 Gui.Timer.prototype.dispatch = function () {
 
-    this.store = VDRest.app.getModule('VDRest.Timer');
+    this.store = this.getStore();
     this.getController('List').dispatchView();
+};
+
+Gui.Timer.prototype.getStore = function () {
+
+    if (!this.store) {
+        this.store = VDRest.app.getModule('VDRest.Timer');
+    }
+    return this.store;
 };
 
 /**
@@ -55,6 +82,21 @@ Gui.Timer.prototype.destruct = function () {
 
     this.getController('List').destructView();
     this.cache.flush();
+};
+
+/**
+ * refresh
+ */
+Gui.Timer.prototype.refresh = function () {
+
+    this.getView('List').node.empty();
+
+    this.getStore().getModel('List').flushCollection();
+    delete this.store.cache.store.Model['List.Timer'];
+    delete this.cache.store.Controller['List.Timer'];
+    delete this.cache.store.View['List.Timer'];
+    delete this.cache.store.ViewModel['List.Timer'];
+    this.dispatch();
 };
 
 /**
